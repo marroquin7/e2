c5584a817593e6d59b84faf58aaa2c2e0e82bd1a
marroquin7 <60824426+marroquin7@users.noreply.github.com>
2020-09-30 20:32:01 -0400

    Project 1 Updated

--- p1/index.php
+++ p1/index.php
@@ -2,23 +2,23 @@
 
 $moves = ['Rock', 'Paper', 'Scissors'];
 
-$Player_A_Move = $moves[rand(0,2)];
-$Player_B_Move = $moves[rand(0,2)];
+$Player_A_Move = $moves[rand(0, 2)];
+$Player_B_Move = $moves[rand(0, 2)];
 
 $result =null;
-if ($Player_A_Move == $Player_B_Move){
+if ($Player_A_Move == $Player_B_Move) {
     $result = 'Tie';
-}elseif($Player_A_Move == 'Paper' and $Player_B_Move=='Rock' ){
+} elseif ($Player_A_Move == 'Paper' and $Player_B_Move=='Rock') {
     $result ='Player A Wins';
-}elseif($Player_A_Move == 'Paper' and $Player_B_Move=='Scissors' ){
+} elseif ($Player_A_Move == 'Paper' and $Player_B_Move=='Scissors') {
     $result ='Player B Wins';
-}elseif($Player_A_Move == 'Rock' and $Player_B_Move=='Paper' ){
+} elseif ($Player_A_Move == 'Rock' and $Player_B_Move=='Paper') {
     $result ='Player B Wins';
-}elseif($Player_A_Move == 'Rock' and $Player_B_Move=='Scissors' ){
+} elseif ($Player_A_Move == 'Rock' and $Player_B_Move=='Scissors') {
     $result ='Player A Wins';
-}elseif($Player_A_Move == 'Scissors' and $Player_B_Move=='Rock' ){
+} elseif ($Player_A_Move == 'Scissors' and $Player_B_Move=='Rock') {
     $result ='Player B Wins';
-}elseif($Player_A_Move == 'Scissors' and $Player_B_Move=='Paper' ){
+} elseif ($Player_A_Move == 'Scissors' and $Player_B_Move=='Paper') {
     $result ='Player A Wins';
 }
 
--- p1/index_view.php
+++ p1/index_view.php
@@ -1,22 +1,27 @@
 <!doctype html>
 <html lang='en'>
-<head><title>Project 1</title>
+
+<head>
+    <title>Project 1</title>
     <meta charset='utf-8'>
 </head>
+
 <body>
     <h1>DGMD E-2 Project 1</h1>
     <b>Author:</b> Cesar Marroquin
     <h2> Mechanics</h2>
     <ul>
-    <li>Player A and Player B randomly “throw” either Rock, Paper or Scissors.</li>
-    <li>A tie is declared if both players throw the same move.</li>
-    <li>Otherwise: Rock beats Scissors, Scissors beats Paper, Paper beats Rock.</li>
+        <li>Player A and Player B randomly “throw” either Rock, Paper or Scissors.</li>
+        <li>A tie is declared if both players throw the same move.</li>
+        <li>Otherwise: Rock beats Scissors, Scissors beats Paper, Paper beats Rock.</li>
     </ul>
     <h2> Results </h2>
     <ul>
-    <li>Player A threw <b><?php echo $Player_A_Move; ?> </b></li>
-    <li>Player B threw <b> <?php echo $Player_B_Move; ?> </b></li>
-    <li>Result: <b> <?php echo $result; ?> </b> </li>
+        <li>Player A threw <b><?php echo $Player_A_Move; ?> </b></li>
+        <li>Player B threw <b> <?php echo $Player_B_Move; ?> </b>
+        </li>
+        <li>Result: <b> <?php echo $result; ?> </b> </li>
     </ul>
 </body>
+
 </html>
