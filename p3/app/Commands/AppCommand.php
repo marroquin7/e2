482d35b37f0be0534ef1b619eca81e4ab9925844
marroquin7 <60824426+marroquin7@users.noreply.github.com>
2020-12-09 22:00:08 -0500

    Project 3 Updated

--- p3/app/Commands/AppCommand.php
+++ p3/app/Commands/AppCommand.php
@@ -8,4 +8,17 @@ class AppCommand extends Command
     {
         dump('It works! You invoked your first command.');
     }
+
+    public function migrate()
+    {
+        $this->app->db()->createTable('rounds', [
+            'player' => 'varchar(255)',
+            'computer' => 'varchar(255)',
+            'outcome' => 'varchar(255)',
+            'timestamp' => 'datetime',
+            
+        ]);
+        
+        dump('Migration complete; check the database for your new tables.');
+    }
 }
